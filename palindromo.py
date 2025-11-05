af523cbca886d3966cef6bdecfe2f9c5273dd7c7
MaSalcedo10 <masg1606@hotmail.com>
2025-11-04 22:01:03 -0500

    Ejercicio Palíndromo

--- palindromo.py
+++ palindromo.py
@@ -1,12 +1,10 @@
-# Detectar palíndromos: crea una función que reciba una cadena y devuelva True si es un palíndromo, ignorando espacios y mayúsculas.
+#	Detectar palíndromos: crea una función que reciba una cadena y devuelva True si es un palíndromo, ignorando espacios y mayúsculas.
 
 def palindromo():
-    cadena = input("Ingrese una cadena: ")
-    
-    cadena_limpia = ''.join(cadena.split()).lower()
-    
+    cadena = input("Ingresa una cadena: ")
+    cadena_limpia = "".join(c.lower() for c in cadena if c.isalnum())
     return cadena_limpia == cadena_limpia[::-1]
 
-resultado = palindromo()
+print("Es palíndromo" if palindromo() else "No es palíndromo")
 
-print(f"Es un palíndromo." if resultado else "No es un palíndromo")
+
