e15b163e78fb2a6646444e89ecc08ab7b02b222f
MaSalcedo10 <masg1606@hotmail.com>
2026-01-06 21:16:08 -0500

    cambios

--- esprimo.py
+++ esprimo.py
@@ -1,11 +1,18 @@
 def esprimo(n):
-    if n<=1:
+    if n <= 1:
         return False
-    for i in range(2, int(n**0.5)+1):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
-resultado = esprimo(17)
+# resultado = esprimo(11)
+# print(resultado)
 
-print(resultado)    
+numero = int(input("Ingrese un numero: "))
+
+if esprimo(numero) == True:
+    print(f"El numero {numero} es primo")
+else:
+    print(f"El numero {numero} no es primo")
+    
